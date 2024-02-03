407b6005aca87348c5871c493f9373083ad1438a
Lucas Tenorio <tnrlucas@gmail.com>
2024-02-03 01:56:31 -0300

    Add metronome sounds

--- src/components/ButtonController.svelte
+++ src/components/ButtonController.svelte
@@ -2,6 +2,6 @@
   export let onClick: () => void;
 </script>
 
-<button on:click={onClick} class="rounded-full bg-white/5 w-14 h-14 text-xl {$$restProps.class || ''}">
+<button on:click={onClick} class="rounded-full bg-white/5 w-14 h-14 text-xl flex justify-center items-center {$$restProps.class || ''}">
   <slot />
 </button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,18 @@
 <script lang="ts">
+	import { onDestroy } from 'svelte';
 	import ButtonController from '../components/ButtonController.svelte';
+	import Play from 'svelte-material-icons/Play.svelte';
+	import Pause from 'svelte-material-icons/Pause.svelte';
+
 	import { browser } from '$app/environment';
 	import { page } from '$app/stores';
 	import { goto } from '$app/navigation';
+	import metronomeMp3 from '../assets/metronome.mp3';
+	import clickMp3 from '../assets/click.mp3';
+	import { Sound } from 'svelte-sound';
+
+	const metronomeSound = new Sound(metronomeMp3);
+	const clickSound = new Sound(clickMp3, { volume: 0.07 });
 
 	const storageKey = 'metronome';
 
@@ -11,6 +21,7 @@
 		const cached = $page.url.searchParams.get(storageKey) || '';
 		return isNaN(+cached) ? 0 : +cached;
 	})();
+	let playing = false;
 
 	// Deal with caching the new metronome value
 	$: (() => {
@@ -19,18 +30,51 @@
 		$page.url.searchParams.set(storageKey, cache);
 		goto($page.url);
 	})();
+	
+	let intervalId: null | number = null;
+	$: (async () => {
+		if (intervalId) {
+			clearInterval(intervalId)
+			intervalId = null;
+		}
+		else {
+			// Start the metronome sound if wasn't playing before
+			metronomeSound.play();
+		}
+		if (playing) {
+			const interval = 60000 / metronome;
+			intervalId = setInterval(async () => {
+				metronomeSound.play();
+			}, interval);
+			return;
+		}
+	})();
+	onDestroy(() => {
+		if (intervalId) clearInterval(intervalId);
+	});
 
 	function increment() {
+		if (!playing) clickSound.play();
 		metronome++;
 	}
 
 	function decrement() {
+		if (!playing) clickSound.play();
 		metronome = Math.max(metronome - 1, 0);
 	}
 </script>
 
-<div class="flex flex-row w-full h-screen items-center justify-center gap-10">
-	<ButtonController class="shrink-0" onClick={decrement}>-</ButtonController>
-	<p>{metronome}</p>
-	<ButtonController class="shrink-0" onClick={increment}>+</ButtonController>
+<div class="flex flex-col w-full h-screen items-center justify-center gap-2">
+	<div class="flex flex-row items-center justify-center gap-10">
+		<ButtonController class="shrink-0" onClick={decrement}>-</ButtonController>
+		<p>{metronome}</p>
+		<ButtonController class="shrink-0" onClick={increment}>+</ButtonController>
+	</div>
+	<ButtonController onClick={() => playing = !playing }>
+		{#if playing}
+		<Pause />
+		{:else}
+		<Play />
+		{/if}
+	</ButtonController>
 </div>
